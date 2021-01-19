a107fb824a86bfa0f7dc42347c552de50b754e92
VanderJunior1 <vanderjunior1@hotmail.com>
2021-01-19 18:08:35 -0300

    Personalizando o retorno da validação

---
+++ src/main/java/br/com/teste/vander/resources/exceptions/FieldMessage.java
@@ -0,0 +1,18 @@
+package br.com.teste.vander.resources.exceptions;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String message;
+
+}
