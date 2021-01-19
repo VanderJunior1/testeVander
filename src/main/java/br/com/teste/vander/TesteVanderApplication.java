40436e79a48ccf8152578a1682acb1065c479c86
VanderJunior1 <vanderjunior1@hotmail.com>
2021-01-19 00:09:15 -0300

    Commit inicial

---
+++ src/main/java/br/com/teste/vander/TesteVanderApplication.java
@@ -0,0 +1,13 @@
+package br.com.teste.vander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteVanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteVanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/teste/vander/TesteVanderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.teste.vander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteVanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
