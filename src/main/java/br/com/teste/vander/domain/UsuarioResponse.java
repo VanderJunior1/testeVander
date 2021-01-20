e4da3a633f3630c0db7cb875a7efe17fd42312ba
VanderJunior1 <vanderjunior1@hotmail.com>
2021-01-19 21:53:36 -0300

    Adicionando dependencias Swagger

--- src/main/java/br/com/teste/vander/domain/UsuarioResponse.java
+++ src/main/java/br/com/teste/vander/domain/UsuarioResponse.java
@@ -11,7 +11,7 @@ public class UsuarioResponse {
 
 	@EqualsAndHashCode.Include
 	private Integer id;
-	private String nome;
+	private String name;
 	private String email;
 	private Boolean ativo;
 }
